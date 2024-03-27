151f0d14d7e8ae61e94e2f3a87f5a21954301c46
Youhei Kirino <8692074+y-krn@users.noreply.github.com>
2024-03-27 17:59:21 +0900

    Update eslint.config.js with new imports and rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
+
 export default [
-    // add more generic rule sets here, such as:
-    // js.configs.recommended,
-    ...eslintPluginAstro.configs['flat/recommended'],
-    {
-        rules: {
-            // override/add rules settings here, such as:
-            // "astro/no-set-html-directive": "error"
-        }
-    }
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+  eslintConfigPrettier,
 ];
