ca8a03004d076eae9a097878e2bd4249c67e4cf9
Youhei Kirino <8692074+y-krn@users.noreply.github.com>
2024-04-28 17:04:05 +0900

    Refactor types.ts to improve code readability and maintainability

--- src/libs/notion/types.ts
+++ src/libs/notion/types.ts
@@ -8,59 +8,70 @@ export type Page = Extract<
   { properties: Record<string, unknown> }
 >;
 
+type ExtractedType<T, K extends keyof T, V extends T[K]> = Extract<
+  T,
+  { [P in K]: V }
+>;
+
 type PropertyValueMap = Page["properties"];
 type PropertyValue = PropertyValueMap[string];
 
-type PropertyValueType = PropertyValue["type"];
-
-type ExtractedPropertyValue<TType extends PropertyValueType> = Extract<
+export type PropertyValueTitle = ExtractedType<PropertyValue, "type", "title">;
+export type PropertyValueRichText = ExtractedType<
   PropertyValue,
-  { type: TType }
+  "type",
+  "rich_text"
+>;
+export type PropertyValueMultiSelect = ExtractedType<
+  PropertyValue,
+  "type",
+  "multi_select"
+>;
+export type PropertyValueUrl = ExtractedType<PropertyValue, "type", "url">;
+export type PropertyValueDate = ExtractedType<PropertyValue, "type", "date">;
+export type PropertyValueEditedTime = ExtractedType<
+  PropertyValue,
+  "type",
+  "last_edited_time"
 >;
 
-export type PropertyValueTitle = ExtractedPropertyValue<"title">;
-export type PropertyValueRichText = ExtractedPropertyValue<"rich_text">;
-export type PropertyValueMultiSelect = ExtractedPropertyValue<"multi_select">;
-export type PropertyValueUrl = ExtractedPropertyValue<"url">;
-export type PropertyValueDate = ExtractedPropertyValue<"date">;
-export type PropertyValueEditedTime =
-  ExtractedPropertyValue<"last_edited_time">;
-
-export type Block =
+export type Block = (
   | Extract<BlockObjectResponse, { type: string }>
-  | ListItemBlock;
+  | ListItemBlock
+) & { children?: Block[] };
 
-export type BlockType = Block["type"];
-
-type ExtractedBlockType<TType extends BlockType> = Extract<
-  Block,
-  { type: TType }
->;
-
-export type ParagraphBlock = ExtractedBlockType<"paragraph">;
-
-export type Heading1Block = ExtractedBlockType<"heading_1">;
-export type Heading2Block = ExtractedBlockType<"heading_2">;
-export type Heading3Block = ExtractedBlockType<"heading_3">;
+export type ParagraphBlock = ExtractedType<Block, "type", "paragraph">;
+export type Heading1Block = ExtractedType<Block, "type", "heading_1">;
+export type Heading2Block = ExtractedType<Block, "type", "heading_2">;
+export type Heading3Block = ExtractedType<Block, "type", "heading_3">;
+export type QuoteBlock = ExtractedType<Block, "type", "quote">;
+export type EquationBlock = ExtractedType<Block, "type", "equation">;
+export type CodeBlock = ExtractedType<Block, "type", "code">;
+export type CalloutBlock = ExtractedType<Block, "type", "callout">;
+export type ToggleBlock = ExtractedType<Block, "type", "toggle">;
+export type EmbedBlock = ExtractedType<Block, "type", "embed">;
+export type BookmarkBlock = ExtractedType<Block, "type", "bookmark">;
+export type ImageBlock = ExtractedType<Block, "type", "image">;
+export type ColumnListBlock = ExtractedType<Block, "type", "column_list">;
+export type ColumnBlock = ExtractedType<Block, "type", "column">;
 
 export type HeadingBlock = Heading1Block | Heading2Block | Heading3Block;
 
-export type ListItemBlock = {
-  type: "bulleted_list" | "numbered_list";
-  blocks: Block[];
-};
-
-export type BulletedListItemBlock = ExtractedBlockType<"bulleted_list_item">;
-export type NumberedListItemBlock = ExtractedBlockType<"numbered_list_item">;
-
-export type QuoteBlock = ExtractedBlockType<"quote">;
-export type EquationBlock = ExtractedBlockType<"equation">;
-export type CodeBlock = ExtractedBlockType<"code">;
-export type CalloutBlock = ExtractedBlockType<"callout">;
-export type ToggleBlock = ExtractedBlockType<"toggle">;
-export type EmbedBlock = ExtractedBlockType<"embed">;
-export type BookmarkBlock = ExtractedBlockType<"bookmark">;
-export type ImageBlock = ExtractedBlockType<"image">;
-
 export type RichText = ParagraphBlock["paragraph"]["rich_text"][number];
 export type File = ImageBlock["image"];
+export type ListItemBlock = {
+  id: string;
+  type: "bulleted_list" | "numbered_list";
+  blocks: Block[];
+  has_children: boolean;
+};
+export type BulletedListItemBlock = ExtractedType<
+  Block,
+  "type",
+  "bulleted_list_item"
+>;
+export type NumberedListItemBlock = ExtractedType<
+  Block,
+  "type",
+  "numbered_list_item"
+>;
