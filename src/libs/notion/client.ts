ddf3302137327e55281cb364e374e2c24805c678
Youhei Kirino <8692074+y-krn@users.noreply.github.com>
2024-04-28 17:04:57 +0900

    Refactor Astro components and add new ones

--- src/libs/notion/client.ts
+++ src/libs/notion/client.ts
@@ -1,7 +1,7 @@
 import { Client } from "@notionhq/client";
 import type { Block, Page, PropertyValueTitle } from "./types";
 
-export async function getPages() {
+export async function getPages(): Promise<Page[]> {
   const notion = new Client({ auth: import.meta.env.NOTION_API_KEY });
   const response = await notion.databases.query({
     database_id: import.meta.env.NOTION_DATABASE_ID,
@@ -15,12 +15,20 @@ export async function getPages() {
   return response.results as Page[];
 }
 
-export async function getPosts(blockId: string) {
+export async function getPosts(blockId: string): Promise<Block[]> {
   const notion = new Client({ auth: import.meta.env.NOTION_API_KEY });
-  const response = await notion.blocks.children.list({
-    block_id: blockId,
-  });
-  return response.results as Block[];
+  const response = await notion.blocks.children.list({ block_id: blockId });
+  return await getChildren(response.results as Block[]);
+}
+
+async function getChildren(blocks: Block[]): Promise<Block[]> {
+  return Promise.all(
+    blocks.map(async (block) => {
+      return block.has_children
+        ? { ...block, children: await getPosts(block.id) }
+        : block;
+    }),
+  );
 }
 
 export async function getTitleProperty(
